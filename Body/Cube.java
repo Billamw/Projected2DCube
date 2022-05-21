e846189ceeb2a61b08d2330e6450d2d7f41c8548
Billa <Billa@users.noreply.github.com>
2022-05-21 21:15:01 +0200

    einiges

--- Body/Cube.java
+++ Body/Cube.java
@@ -12,23 +12,22 @@ public class Cube {
     public Cube(Vec3 pos, int size) {
         this.pos = pos;
         this.size = size;
-        float half = size/2;
 
-        this.a = new Vec3(-half,half,half);
+        this.a = new Vec3(-1,1,1);
         this.a.add(pos);
-        this.b = new Vec3(half,half,half);
+        this.b = new Vec3(1,1,1);
         this.b.add(pos);
-        this.c = new Vec3(half,half,-half);
+        this.c = new Vec3(1,1,-1);
         this.c.add(pos);
-        this.d = new Vec3(-half,half,-half);
+        this.d = new Vec3(-1,1,-1);
         this.d.add(pos);
-        this.e = new Vec3(-half,-half,half);
+        this.e = new Vec3(-1,-1,1);
         this.e.add(pos);
-        this.f = new Vec3(half,-half,half);
+        this.f = new Vec3(1,-1,1);
         this.f.add(pos);
-        this.g = new Vec3(half,-half,-half);
+        this.g = new Vec3(1,-1,-1);
         this.g.add(pos);
-        this.h = new Vec3(-half,-half,-half);
+        this.h = new Vec3(-1,-1,-1);
         this.h.add(pos);
     }
 
--- Main/Main.java
+++ Main/Main.java
@@ -7,7 +7,7 @@ import Main.Draw;
 public class Main {
     public static void main(String[] args) {
 
-        Vec3 pos = new Vec3(200,80,80);
+        Vec3 pos = new Vec3(250,250,0);
         
         Cube cube1 = new Cube(pos, 50);
         float[][] fill = { {1,0,0},
@@ -20,9 +20,9 @@ public class Main {
                           0,1,0,pos.y,
                           0,0,1,pos.z,
                           0,0,0,1);
-        float alpha = (float)Math.toRadians(1);
+        float alpha = (float)Math.toRadians(45);
 
-
+        
         Mat3 r = Mat3.rotation(alpha, alpha, alpha);
 
         Mat4 R = new Mat4(r.m00, r.m01, r.m02, 0, 
@@ -35,24 +35,23 @@ public class Main {
                              0,0,1,-pos.z,
                              0,0,0,1);
 
-        R.mul(TMin);
+        T.mul(R);
         Draw.setBackgroundColor(50, 50, 50);
         Draw.init(500,500);
-        Draw.setFps(30);
-        //Draw.enableDoubleBuffering(true);
         Draw.setColor(255, 255, 255);  
-        while (true){
 
-            if(Draw.waitForKeyboard() == 0x20){Draw.clearScreen();
             
-            for (int i = 0; i < cube1.toArray().length; i++) {
-                //cube1.toArray()[i].showHor();  
+        while(true) {
+
+                for (int i = 0; i < cube1.toArray().length; i++) {
+                cube1.toArray()[i].showHor();  
                 cube1.toArray()[i].mul(Mat3.rotationX(alpha));
                 Draw.filledEllipse(cube1.toArray()[i].mul(orthProject), 5, 5);
             }
-            alpha += Math.toRadians(5);
-            Draw.syncToFrameRate();}
+
         }
+        
+
     }
 
 }
